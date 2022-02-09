226be2989f8d3a7e072ddcbd8dfb6638acf4e335
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-02-09 10:22:09 +0300

    patch javascript file

--- quizboard.js
+++ quizboard.js
@@ -1,10 +1,12 @@
-const Q1A=document.getElementById("1A");
-Q1A.addEventListener("onchange", ()=> {
-    console.log(Q1A)
-});
 
 function check() {
     var A=0;
     var q1=document.quizboard.quiz1.value;
+    var q2=document.quizboard.quiz2.value;
+    var q3=document.quizboard.quiz3.value;
+   
     if (quiz1=="HYPER TEXT MARKUP LANGUAGE") {A++}
-document.write(A);
+    if (quiz1=="ECMA SCRIPT") {A++}
+    if (quiz1=="YES") {A++}
+    
+      document.write(A);
