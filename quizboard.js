322c8ea812ba21a74a5994781ef29f92dbcca6de
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-02-09 10:09:37 +0300

    added javascript

--- quizboard.js
+++ quizboard.js
@@ -1,4 +1,10 @@
-const Q1A=documnet.getElementById("1A");
+const Q1A=document.getElementById("1A");
 Q1A.addEventListener("onchange", ()=> {
     console.log(Q1A)
 });
+
+function check() {
+    var A=0;
+    var q1=document.quizboard.quiz1.value;
+    if (quiz1=="HYPER TEXT MARKUP LANGUAGE") {A++}
+document.write(A);
